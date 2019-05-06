e1e37c6720d99c52da9ca249568ff74c5cca0994
Stepan Nikulenko <stepan.nikulenko@mediamonks.com>
2019-05-06 17:51:44 -0300

    Initial commit with RNNavigation intergated for iOS

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+import { Navigation } from 'react-native-navigation';
+import App from './src/App';
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen',
+      },
+    },
+  });
+});
